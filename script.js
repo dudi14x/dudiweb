66cd90a168a90cb12153bb686b1a3742cf36a889
dudi14x <dudiabduwani@gmail.com>
2026-01-16 03:18:24 +0400

    Initial website upload
    Added contact page for personal website

---
+++ script.js
@@ -0,0 +1,9 @@
+(function () {
+  const path = window.location.pathname.split("/").pop() || "index.html";
+  const links = document.querySelectorAll(".nav-links a");
+
+  links.forEach(a => {
+    const href = a.getAttribute("href");
+    if (href === path) a.classList.add("active");
+  });
+})();
